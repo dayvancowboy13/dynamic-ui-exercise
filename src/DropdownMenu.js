a793e0c1dee819c66dbd882eb43ba0fd1eefa48b
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-07-04 08:10:19 -0700

    tentatively finished with Dropdown menu code; moving on to image carousel

--- src/DropdownMenu.js
+++ src/DropdownMenu.js
@@ -6,18 +6,13 @@ export default class DropdownMenu {
         this.dropdownID = dropdownID;
         this.btn = document.getElementById(this.buttonID);
         this.menu = document.getElementById(this.dropdownID);
+        this.menu.style.position = 'absolute';
 
         let bCoords = this.btn.getBoundingClientRect();
-        this.menu.style.transform = `translateX(${bCoords.x - 5}px)`;
-        console.log(bCoords.x);
+        this.menu.style.left = bCoords.x + 'px';
+        this.menu.style.top = bCoords.y + bCoords.height + 'px';
 
         this.linkButton();
-
-        /*
-         * make dropdown appear below the button
-         * find bottom left of the source button
-         */
-
 
     }
 
@@ -34,54 +29,33 @@ export default class DropdownMenu {
         console.log('linking button');
         const btn = document.getElementById(this.buttonID);
         const menu = document.getElementById(this.dropdownID);
-        // menu.style.display = 'none';
 
         menu.style.visibility = 'hidden';
 
-        // menu.style.transform = 'translateX(500px)';
+        // btn.onclick = () => {
 
-        btn.onclick = () => {
+        //     this.changeVisibility(menu);
 
-            // this.hideElement(menu);
+        // };
 
-            this.changeVisibility(menu);
-
-        };
-        console.log(menu.textContent);
+        btn.addEventListener('click',
+            () => this.changeVisibility(menu));
 
     }
 
-    hideElement(ddID) {
+    changeVisibility(dropDown) {
 
-        if (ddID.style.display !== 'none') {
+        if (dropDown.style.visibility !== 'hidden') {
 
             console.log('hide');
 
-            ddID.style.display = 'none';
+            dropDown.style.visibility = 'hidden';
 
         } else {
 
             console.log('show');
 
-            ddID.style.display = 'block';
-
-        }
-
-    }
-
-    changeVisibility(ddID) {
-
-        if (ddID.style.visibility !== 'hidden') {
-
-            console.log('hide');
-
-            ddID.style.visibility = 'hidden';
-
-        } else {
-
-            console.log('show');
-
-            ddID.style.visibility = 'visible';
+            dropDown.style.visibility = 'visible';
 
         }
 
@@ -89,52 +63,6 @@ export default class DropdownMenu {
 
     static {
 
-        console.log(this.buttonID);
-        console.log('fuck shit');
-
-    }
-
-    static initialize() {
-
-        console.log('starting NavHandler');
-        const button = document.querySelector('.drop-down-btn');
-
-        button.onclick = () => {
-
-            const div = document.querySelector('#menu');
-            div.attributes.st = '';
-
-        };
-
-
-        const btns = document.querySelectorAll('#main-menu');
-
-        console.log(btns);
-        const btn2 = document.getElementById('btn2');
-        const btn2Div = document.getElementById('main-menu');
-        btn2.onclick = () => {
-
-            if (btn2Div.style.visibility !== 'hidden') {
-
-                console.log('hide');
-
-                btn2Div.style.visibility = 'hidden';
-
-            } else {
-
-                console.log('show');
-
-                btn2Div.style.visibility = 'visible';
-
-            }
-
-        };
-
-        /*
-         * HOW TO HIDE/SHOW DIV CONTENT:
-         * style.visibility option
-         * class with separate visibility
-         */
 
     }
 
